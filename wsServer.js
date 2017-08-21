42fb908c8c49c08afc269685f4361652b9a46e07
kiki-lei <358613224@qq.com>
2017-08-21 17:31:44 +0800

    testOne

---
+++ wsServer.js
@@ -0,0 +1,40 @@
+var ws = require("nodejs-websocket");
+var PORT = 3001;
+var userCount = 0;
+
+var server = ws.createServer(function(conn) {
+    console.log("New connection");
+    userCount++;
+    conn.nickname = "User" + userCount;
+    var msg = {};
+    msg.type = "enter";
+    msg.data = conn.nickname + " is coming!";
+    boardcast(JSON.stringify(msg));
+    conn.on('text', function(str) {
+        console.log("Received " + str);
+        var msg = {};
+        msg.type = "message";
+        msg.data = conn.nickname + " says: " + str;
+        boardcast(JSON.stringify(msg));
+    })
+    conn.on('close', function(code, reason) {
+        console.log("connection closed");
+        var msg = {};
+        msg.type = "leave";
+        msg.data = conn.nickname + " is leaving!";
+        boardcast(JSON.stringify(msg));
+    })
+    conn.on('error', function(err) {
+        console.log("handler error!");
+        console.log(err);
+    })
+}).listen(PORT);
+
+console.log("Server is running at port 3001")
+
+//广播信息
+function boardcast(str) {
+    server.connections.forEach(function(connection) {
+        connection.sendText(str);
+    })
+}
